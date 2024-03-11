c9c4c7b557ea73bffe38564490bc50744ea6b98f
gayatriak <52051992+gayatriak@users.noreply.github.com>
2024-03-11 17:11:26 -0500

    quick fix

--- src/finalSolution/conversion.test.js
+++ src/finalSolution/conversion.test.js
@@ -1,4 +1,4 @@
-const temperatureConversions = require('./temperatureConversions');
+const temperatureConversions = require('./conversion');
 
 describe('Temperature Conversions', () => {
   test('Convert 0 Kelvin to Celsius', () => {
